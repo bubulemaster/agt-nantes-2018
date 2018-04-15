04c3300a16b41bd605f86996ffada591c5f3cc28
Emeric <emeric.martineau@fake-email.com>
2018-04-15 18:11:17 +0200

    Rework what is section

--- src/components/whatis/index.js
+++ src/components/whatis/index.js
@@ -1,7 +1,34 @@
 import { Component } from 'preact'
 
 import style from './style'
+
 import grillage from '../grillade-grid'
+
+import whatisList from './whatis-list'
+
+function OneDay(props) {
+	return (
+    <div class={style.whatisDay}>
+  		<div class={[style[props.cssclass], style.whatisDayItem].join(' ')}>
+  		</div>
+			<h3>{props.text}</h3>
+    </div>
+	)
+}
+
+function WhatisList(props) {
+  const whatis = props.whatis
+
+	let listItems = []
+
+	whatis.forEach(day => listItems.push(<OneDay cssclass={day.cssclass} text={day.text} />))
+
+  return (
+		<div>
+    	{listItems}
+		</div>
+  )
+}
 
 export default class Whatis extends Component {
 	render() {
@@ -13,23 +40,20 @@ export default class Whatis extends Component {
 
 				<div class={grillage.autogrid}>
 					<div class={[style.agtDay, style.agtDay1].join(' ')}>
-						<div class={style.agtDayContent}>
-							<h2>Journée #1 : par la pratique</h2>
-							TODO mettre quelques photos qui représente le Dojo Coding, Fishball, Master Class
-							<br />
-							<br />
-							<br />
-							<br />
-							<br />
-							<br />
-							<br />
-							<br />
+						<div class={style.agtDayBackgroundOpacity}>
+							<div class={style.agtDayContent}>
+								<h2>Journée #1 : par la pratique</h2>
+
+								<WhatisList whatis={whatisList.dayOne} />
+							</div>
 						</div>
 					</div>
 					<div class={[style.agtDay, style.agtDay2].join(' ')}>
-						<div class={style.agtDayContent}>
-							<h2>Journée #2 : les sessions</h2>
-							TODO mettre quelques photos qui représente lighting talk, conférence
+						<div class={style.agtDayBackgroundOpacity}>
+							<div class={style.agtDayContent}>
+								<h2>Journée #2 : les sessions</h2>
+								TODO mettre quelques photos qui représente lighting talk, conférence
+							</div>
 						</div>
 					</div>
 				</div>
---
+++ src/components/whatis/whatis-list.js
@@ -0,0 +1,16 @@
+export default {
+  dayOne: [
+    {
+      cssclass: 'masterclass',
+      text: 'Master Class'
+    },
+    {
+      cssclass: 'codingdojo',
+      text: 'Coding Dojo'
+    },
+    {
+      cssclass: 'fishball',
+      text: 'Fishball'
+    }
+  ]
+}
