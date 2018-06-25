7b0673558300801f2e88a4b31b45c3c68e790acd
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2018-06-25 13:25:02 +0200

    Ajout iframe eventbrite

--- src/components/buy/index.js
+++ src/components/buy/index.js
@@ -12,7 +12,16 @@ export default class Buy extends Component {
 					</header>
 				</div>
 
-				Encore un peu de patience ...
+				<div style="width:100%; text-align:left;">
+					<iframe src="https://eventbrite.fr/tickets-external?eid=47351755375&ref=etckt"
+						frameborder="0" height="393" width="100%" vspace="0" hspace="0"
+						marginheight="5" marginwidth="5" scrolling="auto"
+						allowtransparency="true"></iframe>
+
+					<div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;" >
+						<a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="https://www.eventbrite.fr/">Développé par Eventbrite</a>
+					</div>
+				</div>
 			</section>
 		)
 	}
