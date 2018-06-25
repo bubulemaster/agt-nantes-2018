15efd2ab10c1ae5501a0de700c50b1e07f524888
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2018-06-25 13:30:54 +0200

    Mise a jour credit pour image sponsor

--- src/components/credits/credits-list.js
+++ src/components/credits/credits-list.js
@@ -9,7 +9,7 @@ export default [
   'https://pixabay.com/fr/ninja-des-donn%C3%A9es-de-s%C3%A9curit%C3%A9-pc-1507457/',
   'https://commons.wikimedia.org/wiki/File:Masterclass_logo.svg',
   'https://pixabay.com/fr/nuit-cam%C3%A9ra-photographe-canon-1927265/',
-  'https://pixabay.com/fr/monnaie-richesse-finances-%C3%A9pargne-3077900/',
+  'https://pixabay.com/en/concert-crowd-audience-people-768722/',
   'https://pixabay.com/fr/art-oratoire-haut-parleur-l-homme-3159217/',
   'https://pixabay.com/fr/afro-am%C3%A9ricain-afrique-conseil-3155852/',
   'https://pixabay.com/fr/smartphone-technique-de-appareils-3329994/'
