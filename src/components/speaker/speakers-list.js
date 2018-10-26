316e6df33d992e64fb3d99d5fa35d9a42345ce6b
emeric <11473190+bubulemaster@users.noreply.github.com>
2018-10-26 16:13:47 +0200

    Mise a jour liste speaker

--- src/components/speaker/speakers-list.js
+++ src/components/speaker/speakers-list.js
@@ -60,5 +60,264 @@ export default [
     web: 'https://symbiont.io',
     twitter: 'https://twitter.com/dr_c0d3',
     cssclass: 'arnaudBailly'
+  },
+  {
+    name: 'Alvin Berthelot',
+    subtitle: 'Webyousoon',
+    web: 'http://www.webyousoon.com/',
+    twitter: 'https://twitter.com/webyousoon',
+    cssclass: 'alvinBerthelot'
+  },
+  {
+    name: 'Amandine Pasquier',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: 'https://twitter.com/almondyne',
+    cssclass: 'amandinePasquier'
+  },
+  {
+    name: 'Antoine Lelaisant',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: 'https://twitter.com/antoinelelaisan',
+    cssclass: 'antoineLelaisant'
+  },
+  {
+    name: 'Antony Guilloteau',
+    subtitle: 'Fenikso',
+    web: 'https://www.fenikso.fr',
+    twitter: 'https://twitter.com/aguilloteau',
+    cssclass: 'antonyGuilloteau'
+  },
+  {
+    name: 'Aurélien Morvant',
+    subtitle: 'KōKan',
+    web: 'https://www.kokan.fr',
+    twitter: 'https://twitter.com/aurelienmorvant',
+    cssclass: 'aurelienMorvant'
+  },
+  {
+    name: 'Aurélie Robert',
+    subtitle: 'Concerto',
+    web: 'https://conserto.pro',
+    twitter: '',
+    cssclass: 'aurelieRobert'
+  },
+  {
+    name: 'Benjamin Poussin',
+    subtitle: 'Codelutin',
+    web: 'https://www.codelutin.com',
+    twitter: '',
+    cssclass: 'benjaminPoussin'
+  },
+  {
+    name: 'Cécile Especel',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: 'https://twitter.com/cecile_especel',
+    cssclass: 'cecileEspecel'
+  },
+  {
+    name: 'Cécilia Bossard',
+    subtitle: 'Codelutin',
+    web: 'https://www.codelutin.com',
+    twitter: 'https://twitter.com/CeciliaBossard',
+    cssclass: 'ceciliaBossard'
+  },
+  {
+    name: 'Céline Desmons',
+    subtitle: 'Desmons Conseil',
+    web: 'https://www.desmons-conseil.com/',
+    twitter: '',
+    cssclass: 'celineDesmons'
+  },
+  {
+    name: 'Christophe Keromen',
+    subtitle: 'CKTI',
+    web: 'www.ckti.com',
+    twitter: 'https://twitter.com/ckeromen',
+    cssclass: 'christopheKeromen'
+  },
+  {
+    name: 'Christophe Thibaut',
+    subtitle: 'Octo',
+    web: 'https://www.octo.com',
+    twitter: 'https://twitter.com/tof_',
+    cssclass: 'christopheThibaut'
+  },
+  {
+    name: 'Damien Roquel',
+    subtitle: 'Brique24',
+    web: 'http://www.brique24.fr',
+    twitter: 'https://twitter.com/DamienRoquel',
+    cssclass: 'damienRoquel'
+  },
+  {
+    name: 'Damien Thouvenin',
+    subtitle: 'Goood',
+    web: 'https://www.goood.com',
+    twitter: 'https://twitter.com/DamienThouvenin',
+    cssclass: 'damienThouvenin'
+  },
+  {
+    name: "Dorothée Le Seac'h",
+    subtitle: 'AgileGarden',
+    web: 'http://www.agilegarden.fr',
+    twitter: 'https://twitter.com/DoLeSeach',
+    cssclass: 'dorotheeLe_seac_h'
+  },
+  {
+    name: 'Elise Minguet',
+    subtitle: 'Wecollaborate',
+    web: 'http://wecollaborate.fr',
+    twitter: 'https://twitter.com/Elise_Minguet',
+    cssclass: 'eliseMinguet'
+  },
+  {
+    name: 'Emmanuelle Collet',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: '',
+    cssclass: 'emmanuelleCollet'
+  },
+  {
+    name: 'Eve Vinclair-Berkemeier',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: 'https://twitter.com/Evey_Online',
+    cssclass: 'eveVinclair-berkemeier'
+  },
+  {
+    name: 'François-Xavier Maquaire',
+    subtitle: '',
+    web: '',
+    twitter: '',
+    cssclass: 'françois-xavierMaquaire'
+  },
+  {
+    name: 'Fréderic Dufau-Joël',
+    subtitle: 'Indépendant',
+    web: '',
+    twitter: 'https://twitter.com/fdufaujoel',
+    cssclass: 'fredericDufau-joel'
+  },
+  {
+    name: 'Gilles Brieux',
+    subtitle: 'Concerto',
+    web: 'https://conserto.pro',
+    twitter: 'https://twitter.com/viaqbent',
+    cssclass: 'gillesBrieux'
+  },
+  {
+    name: 'Grégoire Robin',
+    subtitle: 'AgileGarden',
+    web: 'http://www.agilegarden.fr',
+    twitter: 'https://twitter.com/gregoirerobin',
+    cssclass: 'gregoireRobin'
+  },
+  {
+    name: 'Nathaniel Richand',
+    subtitle: 'AgileTribu',
+    web: 'http://www.agiletribu.com',
+    twitter: 'https://twitter.com/nrichand',
+    cssclass: 'nathanielRichand'
+  },
+  {
+    name: 'Patrice Boisieau',
+    subtitle: 'HomoAgilis',
+    web: 'http://www.homoagilis.fr/',
+    twitter: 'https://twitter.com/patriceboisieau',
+    cssclass: 'patriceBoisieau'
+  },
+  {
+    name: 'Perrine Baudimont',
+    subtitle: 'Indépendante',
+    web: '',
+    twitter: 'https://twitter.com/pebaudimont',
+    cssclass: 'perrineBaudimont'
+  },
+  {
+    name: 'Pierre Plazanet',
+    subtitle: 'KNP Labs',
+    web: 'http://knplabs.com',
+    twitter: 'https://twitter.com/pedrotroller',
+    cssclass: 'pierrePlazanet'
+  },
+  {
+    name: 'Pierrick Thibault',
+    subtitle: 'AgileGarden',
+    web: 'http://www.agilegarden.fr',
+    twitter: 'https://twitter.com/keurvet?',
+    cssclass: 'pierrickThibault'
+  },
+  {
+    name: 'Raphaël Geneteix',
+    subtitle: '',
+    web: '',
+    twitter: 'https://twitter.com/raphaelgeneteix',
+    cssclass: 'raphaelGeneteix'
+  },
+  {
+    name: 'Romain Couturier',
+    subtitle: 'SuperTilt',
+    web: 'https://www.supertilt.fr/',
+    twitter: 'https://twitter.com/romaincouturier',
+    cssclass: 'romainCouturier'
+  },
+  {
+    name: 'Sam Cranford',
+    subtitle: 'UpWiser',
+    web: 'https://upwiser.com/',
+    twitter: 'https://twitter.com/nostradamnit',
+    cssclass: 'samCranford'
+  },
+  {
+    name: 'Sarah Labaied',
+    subtitle: 'Conseil et vous',
+    web: 'http://www.conseiletvous.fr',
+    twitter: 'https://twitter.com/sarahlabaied',
+    cssclass: 'sarahLabaied'
+  },
+  {
+    name: 'Sébastien Fauvel',
+    subtitle: 'Palo IT',
+    web: 'http://palo-it.com/',
+    twitter: 'https://twitter.com/sebfauvel',
+    cssclass: 'sebastienFauvel'
+  },
+  {
+    name: 'Thomas Nansot',
+    subtitle: 'Living Packets',
+    web: 'https://www.livingpackets.com/',
+    twitter: 'https://twitter.com/tnansot',
+    cssclass: 'thomasNansot'
+  },
+  {
+    name: 'Tremeur Balbous',
+    subtitle: 'Pyxis Tech',
+    web: 'https://pyxis-tech.com',
+    twitter: 'https://twitter.com/agilegardener',
+    cssclass: 'tremeurBalbous'
+  },
+  {
+    name: 'Véronique Lanselle',
+    subtitle: 'Clarifia',
+    web: 'http://clarifia.fr',
+    twitter: 'https://twitter.com/verolanselle',
+    cssclass: 'veroniqueLanselle'
+  },
+  {
+    name: 'Vincent Rostaing',
+    subtitle: 'Le Cairn 4 IT',
+    web: 'http://www.lecairn4it.com/',
+    twitter: 'https://twitter.com/lecairn',
+    cssclass: 'vincentRostaing'
+  },
+  {
+    name: 'Xavier Nopre',
+    subtitle: 'TwinPeek',
+    web: 'https://twinpeek.net/',
+    twitter: 'https://twitter.com/xnopre',
+    cssclass: 'xavierNopre'
   }
 ]
